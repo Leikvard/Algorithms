f77f9d5a649ff0059943a75ae953d36446e8c7a0
Leikvard <wypiao@connect.hku.hk>
2021-08-23 01:00:08 -0400

    modify fibonacci_huge.py

--- fibonacci_huge.py
+++ fibonacci_huge.py
@@ -7,11 +7,16 @@ def get_fibonacci_huge_naive(n, m):
 
     previous = 0
     current  = 1
+    period = [0]
 
     for _ in range(n - 1):
+        period.append(current)
         previous, current = current, (previous + current) % m
+        if current == 0 and previous == 1:
+            break
+    period_length = len(period)
 
-    return current
+    return (period[n % period_length - 1] + period[n % period_length - 2]) % m
 
 if __name__ == '__main__':
     input = sys.stdin.readline();
