986c289b80fbdb5a33b44f317c35edfb9a16c208
Leikvard <wypiao@connect.hku.hk>
2021-08-23 22:28:53 -0400

    recursive with memory

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,14 @@
 def calc_fib(n):
     if (n <= 1):
         return n
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    
+    previous = 0
+    current = 1
+
+    for i in range(n - 1):
+        previous, current = current, previous + current
+
+    return current
 
 n = int(input())
 print(calc_fib(n))
