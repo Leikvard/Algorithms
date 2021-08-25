24314a0e08c6acb152ed9060310b1ee34012ce95
Leikvard <wypiao@connect.hku.hk>
2021-08-24 21:46:16 -0400

    calculate lcm with gcd

--- lcm.py
+++ lcm.py
@@ -1,6 +1,25 @@
 # Uses python3
 import sys
+#get gcd by euclidean algorithm
+def gcd_euclidean(a, b):
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    if a == 1 or b == 1:
+        return 1
+    elif a <= b:
+        return gcd_euclidean(a, b % a)
+    else:
+        return gcd_euclidean(a % b, b)
 
+#get lcm from gcd
+def lcm(a, b):
+    gcd = gcd_euclidean(a, b)
+    return int(a * b / gcd)
+
+
+#naive algorithm
 def lcm_naive(a, b):
     for l in range(1, a*b + 1):
         if l % a == 0 and l % b == 0:
@@ -9,7 +28,7 @@ def lcm_naive(a, b):
     return a*b
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     a, b = map(int, input.split())
-    print(lcm_naive(a, b))
+    print(lcm(a, b))
 
