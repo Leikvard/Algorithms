ea6e237fa4172e6c1d6e0df7482b3256fa61a60c
Leikvard <wypiao@connect.hku.hk>
2021-08-24 21:34:45 -0400

    Euclidean algorithm

--- gcd.py
+++ gcd.py
@@ -1,6 +1,19 @@
 # Uses python3
 import sys
+#Euclidean algorithm
+def gcd_euclidean(a, b):
+    if a == 0:
+        return b
+    if b == 0:
+        return a
+    if a == 1 or b == 1:
+        return 1
+    elif a <= b:
+        return gcd_euclidean(a, b % a)
+    else:
+        return gcd_euclidean(a % b, b)
 
+#naive algorithm
 def gcd_naive(a, b):
     current_gcd = 1
     for d in range(2, min(a, b) + 1):
@@ -11,6 +24,6 @@ def gcd_naive(a, b):
     return current_gcd
 
 if __name__ == "__main__":
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     a, b = map(int, input.split())
     print(gcd_naive(a, b))
