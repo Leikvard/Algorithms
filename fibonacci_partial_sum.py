442d7e26c0b0ae331edcf8ce071df0329f9160fd
Leikvard <wypiao@connect.hku.hk>
2021-08-24 21:58:42 -0400

    calculate from modulo period

--- fibonacci_partial_sum.py
+++ fibonacci_partial_sum.py
@@ -1,6 +1,31 @@
 # Uses python3
 import sys
+#Get period of the last digit of fibonacci number
+def fibo_last_digit_period():
+    previous = 0
+    current  = 1
+    period = [0]
+    flag = not(current == 0 & previous == 1)
+    while flag:
+        period.append(current)
+        previous, current = current, (previous + current) % 10
+        flag = not(current == 0 and previous == 1)
 
+    return period
+period = fibo_last_digit_period()
+period_length = len(period)
+
+#calculate partial sum from period
+#Sum(n) = F(n + 2) - 1
+#F(m) + F(m+1) + ... + Fn = Sum(n) - Sum(m - 1) = F(n + 2) - F(m + 1)
+def partial_sum(m, n):
+    to_last_digit = period[(n + 2) % period_length]
+    from_last_digit = period[(m + 1) % period_length]
+    if to_last_digit - from_last_digit < 0:
+        return 10 + to_last_digit - from_last_digit
+    return to_last_digit - from_last_digit
+
+#naive algorithm
 def fibonacci_partial_sum_naive(from_, to):
     _sum = 0
 
@@ -17,6 +42,6 @@ def fibonacci_partial_sum_naive(from_, to):
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.readline();
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    print(partial_sum(from_, to))
