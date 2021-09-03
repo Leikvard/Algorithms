54026e8619641aab978875c797005c23a6678275
Leikvard <wypiao@connect.hku.hk>
2021-09-02 23:12:00 -0400

    second commit

--- fractional_knapsack.py
+++ fractional_knapsack.py
@@ -2,15 +2,28 @@
 import sys
 
 def get_optimal_value(capacity, weights, values):
-    value = 0.
-    # write your code here
+    unit = [v / m for v, m in zip(values, weights)]
 
-    return value
+    total_value = 0
+    weight_left = capacity
+    
+    while weight_left > 0:
+        unit_max = max(unit)
+        i_max = unit.index(unit_max)
+        weight_max = weights[i_max]
+        putin = min(weight_max, weight_left)
+        weight_left -= putin
+        total_value += unit_max * putin
+
+        unit[i_max] = 0
+
+    return total_value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
+    data = list(map(int, sys.stdin.readline().split()))
     n, capacity = data[0:2]
+#    print(n, capacity, data)
     values = data[2:(2 * n + 2):2]
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
