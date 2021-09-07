fbcdd9931c57b0ce5f5f9e0d6f7a091162e00ba8
Leikvard <wypiao@connect.hku.hk>
2021-09-06 23:38:22 -0400

    second commit

--- covering_segments.py
+++ covering_segments.py
@@ -5,11 +5,23 @@ from collections import namedtuple
 Segment = namedtuple('Segment', 'start end')
 
 def optimal_points(segments):
+    seg_list = []
     points = []
-    #write your code here
     for s in segments:
-        points.append(s.start)
-        points.append(s.end)
+        seg_list.append([s.start, s.end])
+    seg_list.sort()
+    n = len(seg_list)
+    point = -1
+    count = 0
+
+    for i in range(n):
+        if point < seg_list[i][0]:
+            point = seg_list[i][1]
+            points.append(point)
+            count += 1
+        if point > seg_list[i][1]:
+            point = seg_list[i][1]
+            points[count - 1] = point
     return points
 
 if __name__ == '__main__':
@@ -17,5 +29,6 @@ if __name__ == '__main__':
     n, *data = map(int, input.split())
     segments = list(map(lambda x: Segment(x[0], x[1]), zip(data[::2], data[1::2])))
     points = optimal_points(segments)
+    #print(points)
     print(len(points))
     print(*points)
