c0da0df319f635fa1da5dd8765e0e984d6a65de0
Leikvard <wypiao@connect.hku.hk>
2021-09-06 23:38:06 -0400

    fix destimation error

--- car_fueling.py
+++ car_fueling.py
@@ -3,9 +3,24 @@ import sys
 
 
 def compute_min_refills(distance, tank, stops):
-    # write your code here
-    return -1
+    refuel = 0
+    n = len(stops)
+    stops.append(distance)
+    for i in range(n):
+        if stops[i] > m or stops[i+1] - stops[i] > m:
+            return -1
+        if stops[i] == m:
+            refuel += 1
+            stops = [stop - stops[i] for stop in stops]
+        if stops[i] < m:
+            if stops[i+1] > m:
+                refuel += 1
+                stops = [stop - stops[i] for stop in stops]
+        
+    return refuel
+                
 
 if __name__ == '__main__':
     d, m, _, *stops = map(int, sys.stdin.read().split())
+    #print(d, m, stops)
     print(compute_min_refills(d, m, stops))
