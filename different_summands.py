feb4083303271820fe102fc2685def4497bf36b8
Leikvard <wypiao@connect.hku.hk>
2021-09-07 22:09:44 -0400

    algorithm with formula between n and k

--- different_summands.py
+++ different_summands.py
@@ -1,9 +1,15 @@
 # Uses python3
 import sys
-
+#The relationship between n and k is that k^2 + k <= 2n <= k^2 + 3k
 def optimal_summands(n):
     summands = []
-    #write your code here
+    k = 1
+    while not (k ** 2 + k <= 2 * n and k ** 2 + 3 * k >= 2 * n ):
+        summands.append(k)
+        k += 1
+    
+    summands.append(int(n - (k - 1) * k / 2))
+
     return summands
 
 if __name__ == '__main__':
