5454e98233fbcbe57b1fc448a58db713f32cda64
Leikvard <wypiao@connect.hku.hk>
2021-09-14 22:40:31 -0400

    second commit

--- binary_search.py
+++ binary_search.py
@@ -1,15 +1,31 @@
 # Uses python3
 import sys
+import math
 
 def binary_search(a, x):
-    left, right = 0, len(a)
+    left, right = 0, len(a) - 1
+    mid = math.floor((left + right) / 2)
+    while True:
+        if x == a[mid]:
+            return mid
+        if x > a[mid]:
+            left = mid + 1
+            if mid == math.floor((left + right) / 2):
+                return -1
+            mid = math.floor((left + right) / 2)
+        if x < a[mid]:
+            right = mid - 1
+            if mid == math.floor((left + right) / 2):
+                return -1
+            mid = math.floor((left + right) / 2) 
+
     # write your code here
 
-def linear_search(a, x):
-    for i in range(len(a)):
-        if a[i] == x:
-            return i
-    return -1
+def compare_array(A, B):
+    C = [0] * len(A)
+    for i in range(len(B)):
+        C[i] = binary_search(A, B[i])
+    return C
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,4 +35,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
