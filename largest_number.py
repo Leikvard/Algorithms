cc464b8b1156ff2712be06f14da9559bea0b29f1
Leikvard <wypiao@connect.hku.hk>
2021-09-14 21:34:09 -0400

    second commit

--- largest_number.py
+++ largest_number.py
@@ -1,13 +1,24 @@
 #Uses python3
 
 import sys
-
+#auxillary function
+def isGreaterOrEqual(a, b):
+    if int(a + b) >= int(b + a):
+        return True
+    else:
+        return False
+#main function
 def largest_number(a):
     #write your code here
     res = ""
-    for x in a:
-        res += x
-    return res
+    while a:
+        maxDigit = '0'
+        for x in a:
+            if isGreaterOrEqual(x, maxDigit):
+                maxDigit = x
+        res += maxDigit
+        a.remove(maxDigit)
+    return int(res)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
