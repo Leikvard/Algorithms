b49361bc39d144f1d001fca09451821c68b4f8f2
Leikvard <wypiao@connect.hku.hk>
2021-09-26 16:10:50 -0400

    dynamic programing with 2-dimentional matrix

--- lcs2.py
+++ lcs2.py
@@ -1,8 +1,29 @@
 #Uses python3
-
 import sys
+import numpy as np
+
+#Uses python3
+import sys
+import numpy as np
 
 def lcs2(a, b):
+    m = len(a)
+    n = len(b)
+    #creat a m+1 * n+1 two dimentional matrix and set initial value to 0
+    matrix = np.zeros((m + 1) * (n + 1), dtype=np.int64).reshape(m + 1, n + 1)
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            ins = matrix[i - 1, j]
+            dele = matrix[i, j - 1]
+            mis = matrix[i - 1, j - 1]
+            match = matrix[i - 1, j - 1] + 1
+            if a[i - 1] == b[j - 1]:
+                matrix[i, j] = max(ins, dele, match)
+            else:
+                matrix[i, j] = max(ins, dele, mis)				
+    return matrix[i, j]
+
+def lcs(a, b):
     m = len(a) #record the length of sequence a
     n = len(b) #record the length of sequence b
     l = 0 #set initial length of lcs
