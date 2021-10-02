2817f66193c81fa4b012631ede521f9bffec9b89
Leikvard <wypiao@connect.hku.hk>
2021-10-02 00:35:24 -0400

    second commit

--- knapsack.py
+++ knapsack.py
@@ -6,8 +6,8 @@ def optimal_weight(W, w):
     n = len(w)
     # create a two dimensional array
     weight = np.zeros((n + 1) * (W + 1), dtype=np.int64).reshape(n + 1, W + 1)
-    for i in range(1, n + 1):
-        for k in range(1, W + 1):
+    for k in range(1, W + 1):
+        for i in range(1, n + 1):
             weight[i, k] = weight[i - 1, k]
             if w[i - 1] <= k:
                 wgt = weight[i - 1, k - w[i - 1]] + w[i - 1]
